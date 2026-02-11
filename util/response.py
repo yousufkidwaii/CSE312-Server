f2d4382e94d6b5f5bcd9fec016e5869b2812b59d
yousufkidwaii <yousufkidwai245@gmail.com>
2026-02-11 17:48:54 -0500

    commit-3

--- util/response.py
+++ util/response.py
@@ -3,28 +3,68 @@ import json
 
 class Response:
     def __init__(self):
-        pass
+        self.status_code = 200
+        self.status_text = "OK"
+
+        self.header_store = {}
+        self.cookie_store = {}
+        self.body_store = b""
+
 
     def set_status(self, code, text):
-        pass
+        self.status_code = int(code)
+        self.status_text = str(text)
+        return self
 
     def headers(self, headers):
-        pass
+        for k in headers:
+            self.header_store[str(k)] = str(headers[k])
+        return self
 
     def cookies(self, cookies):
-        pass
+        for k in cookies:
+            self.cookie_store[str(k)] = str(cookies[k])
+        return self
 
     def bytes(self, data):
-        pass
+        if not isinstance(data, (bytes, bytearray)):
+            raise TypeError("data must be bytes or bytearray")
+        self.body_store += bytes(data)
+        return self
 
     def text(self, data):
-        pass
+        if not isinstance(data,str):
+            raise TypeError("data must be str")
+        self.body_store += data.encode("utf-8")
+        return self
 
     def json(self, data):
-        pass
+        if not isinstance(data, (dict, list)):
+            raise TypeError("data must be dict or list")
+        json_bytes = json.dumps(data).encode("utf-8")
+        self.body_store += json_bytes
+        self.header_store["Content-Type"] = "application/json"
+        return self
 
     def to_data(self):
-        return b''
+        if "Content-Type" not in self.header_store:
+            self.header_store["Content-Type"] = "text/plain; charset=utf-8"
+        self.header_store["Content-Length"] = str(len(self.body_store))
+        response_lines = []
+
+        status_line = f"HTTP/1.1 {self.status_code} {self.status_text}"
+        response_lines.append(status_line)
+
+        for k in self.header_store:
+            response_lines.append(f"{k}: {self.header_store[k]}")
+
+        for k in self.cookie_store:
+            response_lines.append(f"Set-Cookie: {k}: {self.cookie_store[k]}")
+
+        response_lines.append("")
+        header_bytes = "\r\n".join(response_lines).encode("utf-8")
+
+        return header_bytes + b"\r\n" + self.body_store
 
 
 def test1():
