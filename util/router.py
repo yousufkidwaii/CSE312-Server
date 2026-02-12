f1f47bba5ee83312e069732cbbdd9d1f98b4aad5
yousufkidwaii <yousufkidwai245@gmail.com>
2026-02-12 14:27:57 -0500

    commit-5

--- util/router.py
+++ util/router.py
@@ -1,10 +1,27 @@
+import util.response
 class Router:
 
     def __init__(self):
-        pass
+        self.routes = []
 
     def add_route(self, method, path, action, exact_path=False):
-        pass
+        self.routes.append((method, path, action, exact_path))
 
     def route_request(self, request, handler):
-        pass
+        req_method = request.method
+        req_path = request.path
+
+        for method, path, action, exact_path in self.routes:
+            if method != req_method:
+                continue
+            if exact_path:
+                if req_path == path:
+                    action(request, handler)
+                    return
+            else:
+                if req_path.startswith(path):
+                    action(request,handler)
+                    return
+
+        resp = util.response.Response().set_status(404, "Not Found").text("404 Not Found")
+        handler.request.sendall(resp.to_data())
