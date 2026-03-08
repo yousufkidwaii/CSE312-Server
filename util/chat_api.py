4ffc4e3764036e5ecad175582aaaf3bfd75d1b00
yous <yousufki@buffalo.edu>
2026-03-08 15:28:01 -0400

    hw2-commit18 bruh

--- util/chat_api.py
+++ util/chat_api.py
@@ -326,7 +326,8 @@ def user_login(request,handler):
     res.cookies({
         "auth_token": auth_token,
         "HttpOnly": None,
-        "Max-Age": 30 * 24 * 60 * 60#,
+        "Max-Age": 30 * 24 * 60 * 60,
+        #"Path": "/login"#,
         #"HttpOnly": ""
     })
     handler.request.sendall(res.to_data())
@@ -352,7 +353,8 @@ def user_logout(request, handler):
     res.cookies({
         "auth_token": "",
         "HttpOnly": None,
-        "Max-Age": 0
+        "Max-Age": 0,
+        #"Path": "/"
     })
     handler.request.sendall(res.to_data())
 #lol
